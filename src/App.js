f560915afc2dc046cd3bf3ce8d72faaa56ef845d
Wikanda-Ch <6110110408@psu.ac.th>
2020-08-15 03:18:53 +0700

    part 7-7

--- src/App.js
+++ src/App.js
@@ -2,20 +2,37 @@ import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word1 = "𝐵𝑙𝑎𝑐𝑘𝑃𝑖𝑛𝑘";
+/*const word1 = "𝐵𝑙𝑎𝑐𝑘𝑃𝑖𝑛𝑘";
 const word2 = "8311";
 const word3 = "8217";
-const word4 = "8528";
+const word4 = "8528";*/
+var word = "";
+var quest ="";
 
+var x = Math.floor(Math.random() * 4);
+if (x == 0){
+  quest ="𝐻𝑖𝑛𝑡: 𝑆𝑜𝑢𝑡ℎ 𝐾𝑜𝑟𝑒𝑎𝑛 𝐺𝑖𝑟𝑙 𝐺𝑟𝑜𝑢𝑝 👭🏼👭🏼";
+  word = "𝐵𝑙𝑎𝑐𝑘𝑃𝑖𝑛𝑘";
+}
+if (x == 1){
+  quest ="1. __ x __ + __ - __ = 24";
+  word = "8311";
+}
+if (x == 2){
+  quest ="2. ( __ x __ ) + __ + __ = 24";
+  word = "8311";
+}
+if (x == 3){
+  quest ="3. ( __ x __ ) - ( __ x __ ) = 24";
+  word = "8311";
+}
 
 function App() {
 
-  var x = Math.floor(Math.random() * 4);
-  //var x = 0;
-if(x == 0) {
   return (
     <body>
     <div>
+      
     <center>
       
       <footer><br></br><b><h1><b1>🌈𝑊𝑒𝑙𝑐𝑜𝑚𝑒 𝑡𝑜 𝑡ℎ𝑒 𝑆ℎ𝑢𝑓𝑓𝑙𝑒 𝑔𝑎𝑚𝑒!✨</b1></h1></b><br></br></footer>
@@ -23,6 +40,7 @@ if(x == 0) {
     <br></br> 
     <img src="https://i.pinimg.com/564x/fa/60/19/fa601977af2c5100d69afc3d1f3aff07.jpg" alt="Image"></img>
     <br></br> 
+
     </center>
 
     <center>
@@ -36,9 +54,11 @@ if(x == 0) {
 
     <div id="Question" class="container ">
     <container>
-      <br></br><b2>𝐻𝑖𝑛𝑡: 𝑆𝑜𝑢𝑡ℎ 𝐾𝑜𝑟𝑒𝑎𝑛 𝐺𝑖𝑟𝑙 𝐺𝑟𝑜𝑢𝑝 👭🏼👭🏼</b2>
+
+      <br></br><b2>{quest}</b2>
+      
       <br></br>
-      <WordCard value={word1}/>
+      <WordCard value={word}/>
       <b2>𝑁𝑒𝑥𝑡/𝑆𝑘𝑖𝑝 ☞︎ <a href="http://localhost:3000/">Click</a></b2>
     </container>
     </div>
@@ -46,118 +66,10 @@ if(x == 0) {
     </div>
     </body>   
   );
-  }
+  //}
 
       
-      if(x == 1) {
-        return (
-          <body>
-          <div>
-          <center>
-            
-            <footer><br></br><b><h1><b1>🌈𝑊𝑒𝑙𝑐𝑜𝑚𝑒 𝑡𝑜 𝑡ℎ𝑒 𝑆ℎ𝑢𝑓𝑓𝑙𝑒 𝑔𝑎𝑚𝑒!✨</b1></h1></b><br></br></footer>
       
-          <br></br> 
-          <img src="https://i.pinimg.com/564x/fa/60/19/fa601977af2c5100d69afc3d1f3aff07.jpg" alt="Image"></img>
-          <br></br> 
-          </center>
-      
-          <center>
-          <b2>
-            <br></br>❗𝐶𝑙𝑖𝑐𝑘 𝑡ℎ𝑒 𝑏𝑜𝑡𝑡𝑜𝑛 𝑡𝑜 𝑝𝑙𝑎𝑦 𝑡ℎ𝑒 𝑔𝑎𝑚𝑒. 
-            <br></br>⭕️𝐼𝑓 𝑦𝑜𝑢 𝑠𝑤𝑎𝑝 𝑡𝑟𝑢𝑒,𝑦𝑜𝑢 𝑤𝑖𝑛.
-            <br></br>❌𝐼𝑓 𝑦𝑜𝑢 𝑠𝑤𝑎𝑝 𝑤𝑟𝑜𝑛𝑔,𝑦𝑜𝑢 𝑙𝑜𝑠𝑒 𝑎𝑛𝑑 𝑡ℎ𝑒 𝑔𝑎𝑚𝑒 𝑤𝑖𝑙𝑙 𝑟𝑒𝑠𝑒𝑡.
-          </b2>
-            <br></br><br></br>
-          </center>
-      
-          <div id="Question" class="container ">
-          <container>
-              <br></br><b2>1. __ x __ + __ - __ = 24</b2>
-              <br></br>
-              <WordCard value={word2}/>
-              <b2>𝑁𝑒𝑥𝑡/𝑆𝑘𝑖𝑝 ☞︎ <a href="http://localhost:3000/">Click</a></b2>
-          </container>
-          </div>
-          
-          </div>
-          </body>   
-        );
-        }
-
-
-      if(x == 2) {
-        return (
-          <body>
-          <div>
-          <center>
-            
-            <footer><br></br><b><h1><b1>🌈𝑊𝑒𝑙𝑐𝑜𝑚𝑒 𝑡𝑜 𝑡ℎ𝑒 𝑆ℎ𝑢𝑓𝑓𝑙𝑒 𝑔𝑎𝑚𝑒!✨</b1></h1></b><br></br></footer>
-      
-          <br></br> 
-          <img src="https://i.pinimg.com/564x/fa/60/19/fa601977af2c5100d69afc3d1f3aff07.jpg" alt="Image"></img>
-          <br></br> 
-          </center>
-      
-          <center>
-          <b2>
-            <br></br>❗𝐶𝑙𝑖𝑐𝑘 𝑡ℎ𝑒 𝑏𝑜𝑡𝑡𝑜𝑛 𝑡𝑜 𝑝𝑙𝑎𝑦 𝑡ℎ𝑒 𝑔𝑎𝑚𝑒. 
-            <br></br>⭕️𝐼𝑓 𝑦𝑜𝑢 𝑠𝑤𝑎𝑝 𝑡𝑟𝑢𝑒,𝑦𝑜𝑢 𝑤𝑖𝑛.
-            <br></br>❌𝐼𝑓 𝑦𝑜𝑢 𝑠𝑤𝑎𝑝 𝑤𝑟𝑜𝑛𝑔,𝑦𝑜𝑢 𝑙𝑜𝑠𝑒 𝑎𝑛𝑑 𝑡ℎ𝑒 𝑔𝑎𝑚𝑒 𝑤𝑖𝑙𝑙 𝑟𝑒𝑠𝑒𝑡.
-          </b2>
-            <br></br><br></br>
-          </center>
-      
-          <div id="Question" class="container ">
-          <container>
-              <br></br><b2>2. ( __ x __ ) + __ + __ = 24</b2>
-              <br></br>
-              <WordCard value={word3}/>
-              <b2>𝑁𝑒𝑥𝑡/𝑆𝑘𝑖𝑝 ☞︎ <a href="http://localhost:3000/">Click</a></b2>
-          </container>
-          </div>
-          
-          </div>
-          </body>   
-        );
-        }
-
-
-      if(x == 3) {
-        return (
-          <body>
-          <div>
-          <center>
-            
-            <footer><br></br><b><h1><b1>🌈𝑊𝑒𝑙𝑐𝑜𝑚𝑒 𝑡𝑜 𝑡ℎ𝑒 𝑆ℎ𝑢𝑓𝑓𝑙𝑒 𝑔𝑎𝑚𝑒!✨</b1></h1></b><br></br></footer>
-      
-          <br></br> 
-          <img src="https://i.pinimg.com/564x/fa/60/19/fa601977af2c5100d69afc3d1f3aff07.jpg" alt="Image"></img>
-          <br></br> 
-          </center>
-      
-          <center>
-          <b2>
-            <br></br>❗𝐶𝑙𝑖𝑐𝑘 𝑡ℎ𝑒 𝑏𝑜𝑡𝑡𝑜𝑛 𝑡𝑜 𝑝𝑙𝑎𝑦 𝑡ℎ𝑒 𝑔𝑎𝑚𝑒. 
-            <br></br>⭕️𝐼𝑓 𝑦𝑜𝑢 𝑠𝑤𝑎𝑝 𝑡𝑟𝑢𝑒,𝑦𝑜𝑢 𝑤𝑖𝑛.
-            <br></br>❌𝐼𝑓 𝑦𝑜𝑢 𝑠𝑤𝑎𝑝 𝑤𝑟𝑜𝑛𝑔,𝑦𝑜𝑢 𝑙𝑜𝑠𝑒 𝑎𝑛𝑑 𝑡ℎ𝑒 𝑔𝑎𝑚𝑒 𝑤𝑖𝑙𝑙 𝑟𝑒𝑠𝑒𝑡.
-          </b2>
-            <br></br><br></br>
-          </center>
-      
-          <div id="Question" class="container ">
-          <container>
-              <br></br><b2>3. ( __ x __ ) - ( __ x __ ) = 24</b2>
-              <br></br>
-              <WordCard value={word4}/>
-              <b2>𝑁𝑒𝑥𝑡/𝑆𝑘𝑖𝑝 ☞︎ <a href="http://localhost:3000/">Click</a></b2>
-          </container>
-          </div>
-          
-          </div>
-          </body>   
-        );
-        }
 
 
 
