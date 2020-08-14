23d7777166ff5521b95b0e8135af5d833912cdfc
Wikanda-Ch <6110110408@psu.ac.th>
2020-08-15 02:28:18 +0700

    part 7-6

--- src/WordCard.js
+++ src/WordCard.js
@@ -29,12 +29,12 @@ export default function WordCard(props){
             if(guess == state.word){
                 console.log('You Win!')
                 setState({...state, guess: '', completed: true})
-                messageWin()
+                alert("You Win!")
             }
             else{
                 console.log('reset, next attempt')
                 setState({...state, guess: '', attempt: state.attempt + 1})
-                messageFail()
+                alert("Please try again.")
             }
         }
     }
@@ -51,10 +51,3 @@ export default function WordCard(props){
     );
 }
 
-function messageWin() {      
-        alert("You Win!");  
-}
-
-function messageFail() {      
-    alert("Please try again.");  
-}
